d18ad57088a256dbe6821942b63327dc1667c557
Gideon Sarfo <aemonsarfo@outlook.com>
2023-12-07 05:58:52 -0500

    feat(accordion): refactor static accordion to dynamic component
    This commit introduces a dynamic rendering approach for the Accordion component, replacing hard-coded sections with a v-for loop that iterates over an array of items. Each item can be toggled open or closed with an updated click handler, enhancing interactivity and scalability.
    
    Additionally, scripts from external sources have been removed in favor of using `nuxt-icon`, suggesting a shift to a more modular and maintainable codebase. The Nuxt configuration has been updated to include this new module.
    
    Significant changes:
    - Each accordion item's state is now controlled via Vue's reactivity system (`isOpen`), improving user experience.
    - Removal of inline styles and scripts in favor of class-based styling and handling, promoting better separation of concerns.
    - The addition of the `nuxt-icon` package allows for more convenient and efficient icon usage throughout the application.

--- components/Accordion.vue
+++ components/Accordion.vue
@@ -1,113 +1,73 @@
 <template>
   <div>
-    <div class="relative mb-3">
-      <h6 class="mb-0">
-        <button
-          class="relative flex items-center w-full p-4 font-semibold text-left transition-all ease-in border-b border-solid cursor-pointer border-slate-100 text-slate-700 rounded-t-1 group text-dark-500"
-          data-collapse-target="collapse-1"
-        >
-          <span>What is Material Tailwind?</span>
-          <i
-            class="absolute right-0 pt-1 text-xs fa fa-plus group-open:opacity-0"
-          ></i>
-          <i
-            class="absolute right-0 pt-1 text-xs opacity-0 fa fa-minus group-open:opacity-100"
-          ></i>
-        </button>
-      </h6>
+    <div
+      v-for="(item, index) in items"
+      :key="item.id"
+      :class="{
+        'bg-[#F8FAFC]': item.isOpen,
+        'rounded-xl': item.isOpen,
+        'border-none': item.isOpen,
+      }"
+      class="flex flex-col max-w-[90vw] md:w-[620px] border-b border-[#E2E8F0] p-5 mb-3 gap-3 cursor-pointer"
+    >
       <div
-        data-collapse="collapse-1"
-        class="h-0 overflow-hidden transition-all duration-300 ease-in-out"
+        @click="() => itemOpen(index)"
+        class="font-bold text-left flex justify-between items-center min-w-full"
       >
-        <div class="p-4 text-sm leading-normal text-blue-gray-500/80">
-          We're not always in the position that we want to be at. We're
-          constantly growing. We're constantly making mistakes. We're constantly
-          trying to express ourselves and actualize our dreams.
-        </div>
+        {{ item.label }}
+        <span v-if="!item.isOpen"
+          ><Icon name="material-symbols:keyboard-arrow-down-rounded" size="2em"
+        /></span>
+        <span v-else>
+          <Icon name="material-symbols:keyboard-arrow-up-rounded" size="2em" />
+        </span>
       </div>
-    </div>
-    <div class="relative mb-3">
-      <h6 class="mb-0">
-        <button
-          class="relative flex items-center w-full p-4 font-semibold text-left transition-all ease-in border-b border-solid cursor-pointer border-slate-100 text-slate-700 rounded-t-1 group text-dark-500"
-          data-collapse-target="collapse-2"
-        >
-          <span>How to use Material Tailwind?</span>
-          <i
-            class="absolute right-0 pt-1 text-xs fa fa-plus group-open:opacity-0"
-          ></i>
-          <i
-            class="absolute right-0 pt-1 text-xs opacity-0 fa fa-minus group-open:opacity-100"
-          ></i>
-        </button>
-      </h6>
-      <div
-        data-collapse="collapse-2"
-        class="h-0 overflow-hidden transition-all duration-300 ease-in-out"
+
+      <p
+        :class="{ hidden: !item.isOpen }"
+        class="text-[#64748B] select-none cursor-default"
       >
-        <div class="p-4 text-sm leading-normal text-blue-gray-500/80">
-          We're not always in the position that we want to be at. We're
-          constantly growing. We're constantly making mistakes. We're constantly
-          trying to express ourselves and actualize our dreams.
-        </div>
-      </div>
-    </div>
-    <div class="relative mb-3">
-      <h6 class="mb-0">
-        <button
-          class="relative flex items-center w-full p-4 font-semibold text-left transition-all ease-in border-b border-solid cursor-pointer border-slate-100 text-slate-700 rounded-t-1 group text-dark-500"
-          data-collapse-target="collapse-3"
-        >
-          <span>What can I do with Material Tailwind?</span>
-          <i
-            class="absolute right-0 pt-1 text-xs fa fa-plus group-open:opacity-0"
-          ></i>
-          <i
-            class="absolute right-0 pt-1 text-xs opacity-0 fa fa-minus group-open:opacity-100"
-          ></i>
-        </button>
-      </h6>
-      <div
-        data-collapse="collapse-3"
-        class="h-0 overflow-hidden transition-all duration-300 ease-in-out"
-      >
-        <div class="p-4 text-sm leading-normal text-blue-gray-500/80">
-          We're not always in the position that we want to be at. We're
-          constantly growing. We're constantly making mistakes. We're constantly
-          trying to express ourselves and actualize our dreams.
-        </div>
-      </div>
+        {{ item.content }}
+      </p>
     </div>
   </div>
 </template>
 
-<!-- <script setup>
-const items = [
+<script setup>
+let items = ref([
   {
     label: "What is Wopebozin?",
-    defaultOpen: true,
+    isOpen: false,
     content:
       "Wopeboozin is a platform that connects fans with their favorite content creators, allowing fans to access exclusive premium content and creators to monetize their work.",
   },
   {
     label: "How do I sign up for Wopeboozin?",
+    isOpen: false,
     content:
       "Fans can sign up using their phone number, while creators can also sign up using their phone numbers and complete KYC (Know Your Customer) verification with valid identification documents.",
   },
   {
     label: "Is my personal information safe on Wopeboozin?",
+    isOpen: false,
     content:
       "Fans can sign up using their phone number, while creators' can also sign up using their phone numbers and complete KYC (Know Your Customer) verification with valid identification documents.",
   },
   {
     label: "How do I subscribe to my favorite creators premium content?",
+    isOpen: false,
     content:
       "Once youve signed up, you can explore creators' profiles and subscribe to their premium content by following their instructions on their pages.",
   },
   {
     label: "How much can I make as a Wopebozin Creator",
+    isOpen: false,
     content:
       "The total you can earn on Wopebozin is unlimited, you’re totally in control. With Wopebozin you take home 80% of your earnings.",
   },
-];
-</script> -->
+]);
+
+function itemOpen(index) {
+  items.value[index].isOpen = !items.value[index].isOpen;
+}
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    head: {
-      script: [
-        {
-          async: true,
-          src: "https://unpkg.com/@material-tailwind/html@latest/scripts/Accordion.js",
-        },
-      ],
-    },
-  },
+
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
+  modules: ["nuxt-icon"],
 })
