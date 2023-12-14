d3d8a7a7033d7e090dcfb003cab6c89dde15624a
Gideon Sarfo <aemonsarfo@outlook.com>
2023-12-14 12:32:37 -0500

    feat(about-section): add new About component and update home page
    Added a new About component with corresponding styles and integrated it into the homepage. The About section provides users with information about the platform's purpose of connecting fans and creators. A new placeholder image (AboutPic.png) has been included to accompany the text content. Commented-out sections like Creator, Fan, and Faqs remain unchanged for future use.

--- pages/index.vue
+++ pages/index.vue
@@ -4,10 +4,12 @@ import Hero from "../views/Hero.vue";
 import Faqs from "../views/Faqs.vue";
 import Fan from "../views/Fan.vue";
 import Creator from "../views/Creator.vue";
+import About from "../views/About.vue";
 </script>
 
 <template>
   <Hero />
+  <About />
   <!-- <Creator />
   <Fan />
   <Faqs /> -->
---
+++ views/About.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="flex justify-center mb-40">
+    <section class="max-w-[1440px] flex flex-col overflow-hidden">
+      <p
+        class="font-[PeckhamPressTrial] text-[#FAFAFA] text-[100px] lg:text-[205px] leading-none -translate-x-20 lg:-translate-x-40"
+      >
+        ABOUT
+      </p>
+      <div class="flex flex-col lg:flex-row gap-10 lg:gap-0">
+        <div class="flex flex-col px-5 gap-7">
+          <div class="">
+            <div
+              class="flex gap-2 p-3 rounded-sm shadow-md w-max relative lg:translate-x-40 translate-x-10 translate-y-5 lg:translate-y-5 -rotate-6"
+            >
+              🎥
+              <p>Videos /Images</p>
+            </div>
+            <h1 class="max-w-[656px]">Create, Connect & Earn <span></span></h1>
+          </div>
+          <p class="max-w-[474px] text-[#64748B]">
+            Wopeboozin is the ultimate platform that connects fans and creators
+            in an entirely new way. It's your golden ticket to a world where you
+            can subscribe for exclusive content from your favorite creators, all
+            in one place.
+          </p>
+          <div class="button rounded-[60px] w-max p-3 text-white">
+            <p><span>🙂</span> Join waitlist</p>
+          </div>
+        </div>
+        <div class="flex flex-col self-end">
+          <img src="../assets/AboutPic.png" alt="" class="" />
+        </div>
+      </div>
+    </section>
+  </section>
+</template>
+
+<style scoped>
+.button {
+  background: linear-gradient(92.04deg, #ff3461 14.07%, #fd7965 99.74%);
+}
+</style>
